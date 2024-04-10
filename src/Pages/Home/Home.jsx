c2d482f514045dc7da4f7bb19e1fa368701e1495
Fahmudul Hassan <fahmudul7890@email.com>
2024-04-10 20:34:28 +0600

    added slider image and description

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -6,7 +6,7 @@ import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import "swiper/css/bundle";
 import { Swiper, SwiperSlide } from "swiper/react";
-import './slider.css'
+import "./slider.css";
 // import img_1 from "../../assets/slider 1.png";
 const Home = () => {
   return (
@@ -14,7 +14,8 @@ const Home = () => {
       <Navbar></Navbar>
       {/*Banner section*/}
       <div className="w-[90%] mt-5 mx-auto ">
-        <Swiper className=" h-[900px]"
+        <Swiper
+          className=" h-[900px]"
           // install Swiper modules
           modules={[Navigation, Pagination, Scrollbar, A11y]}
           spaceBetween={50}
@@ -27,21 +28,56 @@ const Home = () => {
         >
           <SwiperSlide>
             <div className=" rounded-2xl min-h-[800px] slider_image_1">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis, excepturi.
+              <div className=" text-white w-[80%] py-[20%] px-[5%]">
+                <h1 className="text-3xl md:text-5xl lg:text-5xl">
+                  Your Private Island Getaway
+                </h1>
+                <p className="lg:w-[60%] text-lg">
+                  Experience ultimate relaxation and seclusion as you unwind in
+                  luxurious villas with breathtaking ocean views. Enjoy
+                  personalized service, gourmet dining, and a range of
+                  activities including snorkeling, scuba diving, and sunset
+                  cruises
+                </p>
+                <button className="btn">Show Details</button>
+              </div>
             </div>
           </SwiperSlide>
           <SwiperSlide>
-            <div className=" rounded-2xl min-h-[800px] slider_image_1">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis, excepturi.
+            <div className=" rounded-2xl min-h-[800px] slider_image2">
+              <div className=" text-white w-[80%] py-[20%] px-[5%]">
+                <h1 className="text-3xl md:text-5xl lg:text-5xl">
+                  Escape to Paradise: Discover Our Exquisite Resort Experience
+                </h1>
+                <p className="lg:w-[60%] text-lg">
+                  Immerse yourself in the ultimate getaway experience at
+                  Paradise Resort, where every moment is designed to captivate
+                  your senses and rejuvenate your spirit. Nestled amidst the
+                  lush, tropical landscapes of [location], our resort offers a
+                  harmonious blend of luxury, relaxation, and excitement.
+                </p>
+                <button className="btn">Show Details</button>
+              </div>
             </div>
           </SwiperSlide>
           <SwiperSlide>
-            <div className=" rounded-2xl min-h-[800px] slider_image_1">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis, excepturi.
+            <div className=" rounded-2xl min-h-[800px] slider_image_3">
+              <div className=" text-white w-[80%] py-[20%] px-[5%]">
+                <h1 className="text-3xl md:text-5xl lg:text-5xl">
+                  Ascend to Luxury: Discover Our Exquisite Penthouse Collection
+                </h1>
+                <p className="lg:w-[60%] text-lg">
+                  Welcome to unparalleled luxury living at our exquisite
+                  penthouse. Perched high above the city skyline, this
+                  magnificent residence offers an unparalleled blend of
+                  opulence, comfort, and breathtaking views. Designed for those
+                  with discerning tastes, our penthouse redefines urban
+                  sophistication.
+                </p>
+                <button className="btn">Show Details</button>
+              </div>
             </div>
           </SwiperSlide>
-          
-          
         </Swiper>
       </div>
     </div>
